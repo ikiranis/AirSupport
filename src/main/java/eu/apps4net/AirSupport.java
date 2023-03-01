f16951ab02dec48f7a69205f5adcffc9d22bf14f
rocean <rocean74@gmail.com>
2023-03-01 02:41:25 +0200

    Get tweet in class format

--- src/main/java/eu/apps4net/AirSupport.java
+++ src/main/java/eu/apps4net/AirSupport.java
@@ -1,11 +1,12 @@
 package eu.apps4net;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -15,16 +16,65 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class AirSupport {
 
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, FloatWritable> {
+    /**
+     * This method uses a regular expression to split each line to a list of strings,
+     * each one representing one column
+     */
+    private static String[] processLine(String line) {
+        // Create a regular expression for proper split of each line
 
-        private final static FloatWritable wordLength = new FloatWritable();
+        // The regex for characters other than quote (")
+        String otherThanQuote = " [^\"] ";
+
+        // The regex for a quoted string. e.g "whatever1 whatever2"
+        String quotedString = String.format(" \" %s* \" ", otherThanQuote);
+
+        // The regex to split the line using comma (,) but taking into consideration the quoted strings
+        // This means that is a comma is in a quoted string, it should be ignored.
+        String regex = String.format("(?x) " + // enable comments, ignore white spaces
+                        ",                         " + // match a comma
+                        "(?=                       " + // start positive look ahead
+                        "  (?:                     " + //   start non-capturing group 1
+                        "    %s*                   " + //     match 'otherThanQuote' zero or more times
+                        "    %s                    " + //     match 'quotedString'
+                        "  )*                      " + //   end group 1 and repeat it zero or more times
+                        "  %s*                     " + //   match 'otherThanQuote'
+                        "  $                       " + // match the end of the string
+                        ")                         ", // stop positive look ahead
+                otherThanQuote, quotedString, otherThanQuote);
+        String[] tokens = line.split(regex, -1);
+
+        // check for the proper number of columns
+        if (tokens.length == 10) {
+            return tokens;
+        } else {
+            System.err.println("Wrong number of columns for line: " + line);
+            return null;
+        }
+    }
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable();
         private final Text word = new Text();
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            Tweet tweet;
+
             String line = value.toString();
 
             // Αφαίρεση σημείων στίξης και μετατροπή σε lower case
-            line = line.replaceAll("\\p{Punct}", " ").toLowerCase();
+            line = line.toLowerCase();
+
+            System.out.println(Arrays.toString(processLine(line)));
+            String[] tweetArray = processLine(line);
+
+            if(tweetArray != null) {
+                tweet = new Tweet(tweetArray[0], tweetArray[1], tweetArray[2], tweetArray[3], tweetArray[4], tweetArray[5], tweetArray[6], tweetArray[7], tweetArray[8], tweetArray[9]);
+
+                System.out.println(tweet);
+            }
+
 
             StringTokenizer itr = new StringTokenizer(line);
             while (itr.hasMoreTokens()) {
@@ -33,39 +83,27 @@ public class AirSupport {
 
                 // Αν δεν αρχίζει από αριθμό
                 if(!token.matches("^\\d.*")) {
-                    word.set(String.valueOf(token.charAt(0)));
+                    word.set(String.valueOf(token));
 
-                    wordLength.set(token.length());
-
-                    context.write(word, wordLength);
+                    context.write(word, one);
                 }
 
             }
         }
     }
 
-    public static class AvgReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> {
-        private final FloatWritable result = new FloatWritable();
+    public static class AvgReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private final IntWritable result = new IntWritable();
 
-        public void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
-            int count = 0;
 
-//            System.out.print(key + " ");
-
-            for (FloatWritable val : values) {
+            for (IntWritable val : values) {
                 sum += val.get();
-                count++;
             }
 
-            float average = (float) sum/count;
-//            System.out.println("Sum: " + sum + " count: " + count + " average: "  + average);
-
-            // Format the average value as a string with two decimal places
-            String formattedAverage = String.format("%.1f", average);
-
             // Parse the formatted string back into a float
-            result.set(Float.parseFloat(formattedAverage));
+            result.set(sum);
 
             context.write(key, result);
         }
@@ -73,14 +111,100 @@ public class AirSupport {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "Average words length");
+        Job job = Job.getInstance(conf, "Airline tweets");
         job.setJarByClass(AirSupport.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setReducerClass(AvgReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(FloatWritable.class);
+        job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+
+    /**
+     * Βοηθητική κλάση για την καταχώρηση του Tweet
+     */
+    public static class Tweet {
+        private final String tweetId;
+        private final String airlineSentiment;
+        private final String airlineSentimentConfidence;
+        private final String negativeReason;
+        private final String negativeReasonConfidence;
+        private final String airline;
+        private final String name;
+        private final String text;
+        private final String tweetCreated;
+        private final String userTimezone;
+
+        public Tweet(String tweetId, String airlineSentiment, String airlineSentimentConfidence, String negativeReason, String negativeReasonConfidence, String airline, String name, String text, String tweetCreated, String userTimezone) {
+            this.tweetId = tweetId;
+            this.airlineSentiment = airlineSentiment;
+            this.airlineSentimentConfidence = airlineSentimentConfidence;
+            this.negativeReason = negativeReason;
+            this.negativeReasonConfidence = negativeReasonConfidence;
+            this.airline = airline;
+            this.name = name;
+            this.text = text;
+            this.tweetCreated = tweetCreated;
+            this.userTimezone = userTimezone;
+        }
+
+        public String getTweetId() {
+            return tweetId;
+        }
+
+        public String getAirlineSentiment() {
+            return airlineSentiment;
+        }
+
+        public String getAirlineSentimentConfidence() {
+            return airlineSentimentConfidence;
+        }
+
+        public String getNegativeReason() {
+            return negativeReason;
+        }
+
+        public String getNegativeReasonConfidence() {
+            return negativeReasonConfidence;
+        }
+
+        public String getAirline() {
+            return airline;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public String getTweetCreated() {
+            return tweetCreated;
+        }
+
+        public String getUserTimezone() {
+            return userTimezone;
+        }
+
+        @Override
+        public String toString() {
+            return "Tweet{" +
+                    "tweetId='" + tweetId + '\'' +
+                    ", airlineSentiment='" + airlineSentiment + '\'' +
+                    ", airlineSentimentConfidence='" + airlineSentimentConfidence + '\'' +
+                    ", negativeReason='" + negativeReason + '\'' +
+                    ", negativeReasonConfidence='" + negativeReasonConfidence + '\'' +
+                    ", airline='" + airline + '\'' +
+                    ", name='" + name + '\'' +
+                    ", text='" + text + '\'' +
+                    ", tweetCreated='" + tweetCreated + '\'' +
+                    ", userTimezone='" + userTimezone + '\'' +
+                    '}';
+        }
     }
 }
